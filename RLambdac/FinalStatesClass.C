7b9e51ab3b31f55874b4129c9a57ad4b513d0ab0
ansonkwokth <ansonwos@gmail.com>
2022-03-11 15:49:04 +0800

    moved final states out from main.

---
+++ RLambdac/FinalStatesClass.C
@@ -0,0 +1,12 @@
+#pragma once
+#include "Rtypes.h"
+
+struct iFinalStates {
+    Int_t foundFromC = 0;
+    Int_t foundAll = 0;
+    Int_t iP = 99999;
+    Int_t iK = 99999;
+    Int_t iPi = 99999;
+    Int_t iMu = 99999;
+};
+
