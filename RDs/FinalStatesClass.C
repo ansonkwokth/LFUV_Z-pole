237b58fd860122295af04419cb6eef704c633e0c
ansonkwokth <ansonwos@gmail.com>
2022-03-14 10:16:01 +0800

    moved final states out.

---
+++ RDs/FinalStatesClass.C
@@ -0,0 +1,12 @@
+#pragma once
+#include "Rtypes.h"
+
+struct iFinalStates {
+    Int_t foundFromC = 0;
+    Int_t foundAll = 0;
+    Int_t iKNeg = 99999;
+    Int_t iKPos = 99999;
+    Int_t iPi = 99999;
+    Int_t iMu = 99999;
+    Int_t iPho = 99999;
+};
--- RDs/allinone.C
+++ RDs/allinone.C
@@ -6,6 +6,7 @@
 using namespace std;
 
 #include "FeaturesClass.C"
+#include "FinalStatesClass.C"
 #include "Rtypes.h"
 #include "TChain.h"
 #include "TClonesArray.h"
@@ -32,18 +33,6 @@ class ExRootResult;
 class ExRootTreeReader;
 #endif
 */
-
-//---------- find the final states of the decay {{{
-struct iFinalStates {
-    Int_t foundFromC = 0;
-    Int_t foundAll = 0;
-    Int_t iKNeg = 99999;
-    Int_t iKPos = 99999;
-    Int_t iPi = 99999;
-    Int_t iMu = 99999;
-    Int_t iPho = 99999;
-};
-//}}}
 
 //---------- calculating the distance from the origin {{{
 Float_t
